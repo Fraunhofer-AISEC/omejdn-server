6732b28d35ba71f93f79dd15a512a447605f4e94
Thomas Bellebaum <thomas.bellebaum@aisec.fraunhofer.de>
2021-10-29 13:53:44 +0200

    One script - all tests For convenience

---
+++ scripts/test_all.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+testfiles="$(ls -1 tests/test_*.rb)"
+
+for file in $testfiles; do
+	ruby "$file"
+	if [ "$?" -ne "0" ]; then
+		echo "Error: Test $file unsucessful"
+		exit 1
+	fi
+done
+
+echo "All tests passed."
